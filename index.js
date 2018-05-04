cbe9676d33145ccc49ef64afb2ad93df2a3058fe
Derek Silva <dereksilvamusic@gmail.com>
2018-05-04 18:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
  return("I can't hear you!")
  if (string === string.toUpperCase())
  var upperCase = string.toUpperCase();
- return("YES INDEED!");
+ return("YES INDEED!")
  if (string === string("I love you, Grandma."))
  return("I love you too.");
 }
